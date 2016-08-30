7f75300d4e0031802341195c0ce2dbb21d85a357
Алексей <>
2016-08-30 18:26:08 +0300

    migration

---
+++ migrations/m160830_152535_base.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160830_152535_base extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160830_152535_base cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
